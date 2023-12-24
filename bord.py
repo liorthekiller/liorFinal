61d1a92de489c22b30f8b7515a41feeb77e59e41
NOYA12345 <noyatavori15@gmail.com>
2023-12-24 14:23:08 +0200

    display pieces work clicking working

--- bord.py
+++ bord.py
@@ -1,60 +1,98 @@
 import tkinter as tk
+from PIL import Image, ImageTk
 
-from pieces import Rook, Knight, Bishop
-
-
-def on_square_clicked(x, y):
-    # Handle the logic when a square is clicked
-    # Check if it's a valid move, move the piece, switch turns, etc.
-    pass
-
-# Initialize the board with pieces
-board = [[None for _ in range(8)] for _ in range(8)]
-# board[0][0] = Rook("black")
-# board[0][1] = Knight("black")
-# ... Initialize the rest of the board
-
-# Tkinter GUI code to create the board and bind click events
-# ...
+from pieces import Rook, Bishop, Knight, Pawn
 
 
+class Chessboard(tk.Tk):
+    def __init__(self):
+        super().__init__()
+        self.title("Chessboard")
+        self.geometry("400x400")
 
+        self.canvas = tk.Canvas(self, width=400, height=400)
+        self.canvas.pack()
 
+        # Load images for all pieces
+        self.images = {
+            'b_rook': ImageTk.PhotoImage(Image.open('sprites/b.rook.png')),
+            'b_knight': ImageTk.PhotoImage(Image.open('sprites/b.knight.png')),
+            'b_bishop': ImageTk.PhotoImage(Image.open('sprites/b.bishop.png')),
+            'b_queen': ImageTk.PhotoImage(Image.open('sprites/b.queen.png')),
+            'b_king': ImageTk.PhotoImage(Image.open('sprites/b.king.png')),
+            'b_pawn': ImageTk.PhotoImage(Image.open('sprites/b.pawn.png')),
+            'w_rook': ImageTk.PhotoImage(Image.open('sprites/w.rook.png')),
+            'w_knight': ImageTk.PhotoImage(Image.open('sprites/w.knight.png')),
+            'w_bishop': ImageTk.PhotoImage(Image.open('sprites/w.bishop.png')),
+            'w_queen': ImageTk.PhotoImage(Image.open('sprites/w.queen.png')),
+            'w_king': ImageTk.PhotoImage(Image.open('sprites/w.king.png')),
+            'w_pawn': ImageTk.PhotoImage(Image.open('sprites/w.pawn.png'))
+        }
 
-def create_board(window, board_data):
-    board_frame = tk.Frame(window)
-    colors = ["white", "gray"]
+        for row in range(8):
+            for col in range(8):
+                x0 = col * 50
+                y0 = row * 50
+                color = "light grey" if (row + col) % 2 == 0 else "white"
+                self.canvas.create_rectangle(x0, y0, x0+50, y0+50, fill=color)
 
-    for row in range(8):
+        # Place all pieces
+        self.place_pieces()
+        self.create_data_board()
+
+        self.canvas.bind("<Button-1>", self.on_canvas_click)
+
+    def place_pieces(self):
+        # Place black pieces
         for col in range(8):
-            color = colors[(row + col) % 2]
-            square = tk.Frame(board_frame, bg=color, width=60, height=60)
-            square.grid(row=row, column=col)
+            self.canvas.create_image(col * 50 + 25, 1 * 50 + 25, image=self.images['b_pawn'])
+        self.canvas.create_image(0 * 50 + 25,  25, image=self.images['b_rook'])
+        self.canvas.create_image(7 * 50 + 25,  25, image=self.images['b_rook'])
+        self.canvas.create_image(1 * 50 + 25,  25, image=self.images['b_knight'])
+        self.canvas.create_image(6 * 50 + 25,  25, image=self.images['b_knight'])
+        self.canvas.create_image(2 * 50 + 25,  25, image=self.images['b_bishop'])
+        self.canvas.create_image(5 * 50 + 25,  25, image=self.images['b_bishop'])
+        self.canvas.create_image(3 * 50 + 25,  25, image=self.images['b_queen'])
+        self.canvas.create_image(4 * 50 + 25,  25, image=self.images['b_king'])
 
-            piece = board_data[row][col]
-            if piece is not None:
-                label = tk.Label(square, image=piece.image)
-                label.pack()
+        # Place white pieces
+        for col in range(8):
+            self.canvas.create_image(col * 50 + 25, 6 * 50 + 25, image=self.images['w_pawn'])
+        self.canvas.create_image(0 * 50 + 25, 7 * 50 + 25, image=self.images['w_rook'])
+        self.canvas.create_image(7 * 50 + 25, 7 * 50 + 25, image=self.images['w_rook'])
+        self.canvas.create_image(1 * 50 + 25, 7 * 50 + 25, image=self.images['w_knight'])
+        self.canvas.create_image(6 * 50 + 25, 7 * 50 + 25, image=self.images['w_knight'])
+        self.canvas.create_image(2 * 50 + 25, 7 * 50 + 25, image=self.images['w_bishop'])
+        self.canvas.create_image(5 * 50 + 25, 7 * 50 + 25, image=self.images['w_bishop'])
+        self.canvas.create_image(3 * 50 + 25, 7 * 50 + 25, image=self.images['w_queen'])
+        self.canvas.create_image(4 * 50 + 25, 7 * 50 + 25, image=self.images['w_king'])
 
-    board_frame.pack()
 
-def main():
-    window = tk.Tk()
-    window.title("Chess Game")
+    def create_data_board(self):
+        global board_data
+        board_data = [[None for _ in range(8)] for _ in range(8)]
+        board_data[0][0] = Rook("black")
+        board_data[0][1] = Knight("black")
+        board_data[0][2] = Bishop("black")
+        # board_data[1][0] = Pawn("black")
+        # board_data[0][2] = Bishop("black")
+        # board_data[0][2] = Bishop("black")
+        # board_data[0][2] = Bishop("black")
+        # board_data[0][2] = Bishop("black")
+        # board_data[0][2] = Bishop("black")
 
-    # Initialize the board with pieces
-    board_data = [[None for _ in range(8)] for _ in range(8)]
-    board_data[0][0] = Rook("black")
-    # board_data[0][1] = Knight("black")
-    board_data[0][2] = Bishop("black")
-    # board_data[0][2] = Bishop("black")
-    # board_data[0][2] = Bishop("black")
-    # board_data[0][2] = Bishop("black")
-    # board_data[0][2] = Bishop("black")
-    # board_data[0][2] = Bishop("black")
-    # board_data[0][2] = Bishop("black")
-    create_board(window, board_data)
-    window.mainloop()
+    def on_canvas_click(self, event):
+        col = event.x // 50
+        row = event.y // 50
+        print(f"Clicked on square: Row {row}, Column {col}")
+        print(str(type(board_data[row][col])))
+        valids = board_data[row][col].valid_moves(board_data,row,col)
+        print(valids)
+        for x, y in valids:
+            color = "light blue"
+            self.canvas.create_rectangle(x, y, x + 50, y + 50, fill=color)
+
 
 if __name__ == "__main__":
-    main()
+    app = Chessboard()
+    app.mainloop()
--- pieces.py
+++ pieces.py
@@ -11,6 +11,9 @@ class ChessPiece:
         # Override in subclasses to return valid moves from position (x, y)
         pass
 
+    def __str__(self):
+        return self.color + " " + str(type(self))
+
 
 class Pawn(ChessPiece):
     def valid_moves(self, board, x, y):
@@ -19,37 +22,44 @@ class Pawn(ChessPiece):
 
 # Similarly, create classes for Rook, Knight, Bishop, Queen, King
 
-# class Pawn(ChessPiece):
-#     def __init__(self, color):
-#         super().__init__(color)
-#
-#
-#
-#     def valid_moves(self, board, x, y):
-#         direction = 1 if self.color == "white" else -1
-#         start_row = 1 if self.color == "white" else 6
-#         valid_moves = []
-#
-#         # Forward move
-#         if board[x + direction][y] is None:
-#             valid_moves.append((x + direction, y))
-#             # Double move on first move
-#             if x == start_row and board[x + 2 * direction][y] is None:
-#                 valid_moves.append((x + 2 * direction, y))
-#
-#         # Diagonal captures
-#         for dy in [-1, 1]:
-#             if 0 <= y + dy < 8 and board[x + direction][y + dy] is not None:
-#                 if board[x + direction][y + dy].color != self.color:
-#                     valid_moves.append((x + direction, y + dy))
-#
-#         return valid_moves
+class Pawn(ChessPiece):
+    def __init__(self, color):
+        super().__init__(color)
+        super().__init__(color)
+        image_path = 'sprites/b.pawn.png'  # Use the correct path for the bishop image
+        image = Image.open(image_path)
+        self.image = ImageTk.PhotoImage(image)
+
+
+    def valid_moves(self, board, x, y):
+        direction = 1 if self.color == "white" else -1
+        start_row = 1 if self.color == "white" else 6
+        valid_moves = []
+
+        # Forward move
+        if board[x + direction][y] is None:
+            valid_moves.append((x + direction, y))
+            # Double move on first move
+            if x == start_row and board[x + 2 * direction][y] is None:
+                valid_moves.append((x + 2 * direction, y))
+
+        # Diagonal captures
+        for dy in [-1, 1]:
+            if 0 <= y + dy < 8 and board[x + direction][y + dy] is not None:
+                if board[x + direction][y + dy].color != self.color:
+                    valid_moves.append((x + direction, y + dy))
+
+        return valid_moves
 
 
 
 class Knight(ChessPiece):
     def __init__(self, color):
         super().__init__(color)
+        super().__init__(color)
+        image_path = 'sprites/b.knight.png'  # Use the correct path for the bishop image
+        image = Image.open(image_path)
+        self.image = ImageTk.PhotoImage(image)
 
     def valid_moves(self, board, x, y):
         # Generate potential moves for the knight
@@ -107,7 +117,7 @@ class Bishop(ChessPiece):
     def __init__(self, color):
         super().__init__(color)
         super().__init__(color)
-        image_path = 'sprites/b.bishop.png'  # Use the correct path for the bishop image
+        image_path = 'sprites/b.bishop0.png'  # Use the correct path for the bishop image
         image = Image.open(image_path)
         self.image = ImageTk.PhotoImage(image)
 
@@ -137,6 +147,11 @@ class Bishop(ChessPiece):
 class Queen(ChessPiece):
     def __init__(self, color):
         super().__init__(color)
+        super().__init__(color)
+        image_path = 'sprites/b.queen.png'  # Use the correct path for the bishop image
+        image = Image.open(image_path)
+        self.image = ImageTk.PhotoImage(image)
+
 
     def valid_moves(self, board, x, y):
         # Combines the logic of rook and bishop
@@ -146,6 +161,10 @@ class Queen(ChessPiece):
 class King(ChessPiece):
     def __init__(self, color):
         super().__init__(color)
+        super().__init__(color)
+        image_path = 'sprites/b.king.png'  # Use the correct path for the bishop image
+        image = Image.open(image_path)
+        self.image = ImageTk.PhotoImage(image)
 
     def valid_moves(self, board, x, y):
         valid_moves = []
